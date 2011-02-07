12cd7431f4d4d983aa949bd72fdc94050c583060
Felipe Kamakura <felipekamakura@gmail.com>
2011-02-07 00:49:19 -0200

    Commit inicial

---
+++ src/main/scala/code/snippet/HelloWorld.scala
@@ -0,0 +1,17 @@
+package code {
+package snippet {
+
+import _root_.scala.xml.{NodeSeq, Text}
+import _root_.net.liftweb.util._
+import _root_.net.liftweb.common._
+import _root_.java.util.Date
+import code.lib._
+import Helpers._
+
+class HelloWorld {
+  // Altere a linha abaixo para sua frase favorita
+  def howdy = "#greet *" #> "Bem vindo forasteiro"
+}
+
+}
+}
